076248c2fcf2abe5de1e90f8d7722d9d685d46aa
jamborski-dev <jamborski.webdesign@gmail.com>
2024-11-01 22:07:13 +0000

    base url

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -1,8 +1,10 @@
 import axios from "axios"
 
+const serverUrl = process.env.VITE_SERVER_URL
+
 // Create an instance of Axios with default configuration
 export const API = axios.create({
-  baseURL: "http://localhost:5001/api", // Base URL for your API
+  baseURL: `${serverUrl}/api`, // Base URL for your API
   // baseURL: "http://192.168.1.60:5001/api", // Base URL for your API
   timeout: 10000, // Optional: Request timeout in milliseconds
   headers: {
